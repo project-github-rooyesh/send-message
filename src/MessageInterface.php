eade0131920ea292f77570954dee3c438f45f21a
fahime shahzeidi <shahzeydyfah@gmail.com>
2022-02-20 12:40:07 +0330

    add excel

--- src/MessageInterface.php
+++ src/MessageInterface.php
@@ -3,7 +3,6 @@ namespace Esmaili\Message;
 use Esmaili\Message\Imports\MessageImport;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
-//use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Http;
 use Kavenegar\KavenegarApi;
 use Esmaili\Message\Models\Message;
@@ -27,37 +26,45 @@ class MessageInterface
         $this->list_send = $list_send;
         $this->swap_name = $swap_name;
         $this->follow_name = $follow_name;
-//        $this->createMessage();
     }
 
 
-    public static function create($user_id,$message,$token,$list_send)
-    {
-        return new MessageInterface($user_id,$message,$token,$list_send);
-    }
-
-
-
-//////create
-    public function createMessage(){
+    /**
+     * send message in 0098 or kavenegar and store on table
+     *
+     * @param $array
+     * @return bool
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    protected function sendMesssage($array){
         $config = Arr::get(config(),'message',false);
         if ($config){
-            foreach ($this->list_send as $list){
-                if (is_null($list['name'])){
+            foreach ($array as $list){
+                if (!isset($list['name']) || is_null($list['name'])){
                     $message =  str_replace("%name", $this->swap_name.' '.$this->follow_name.' ', $this->message);
                 }else{
                     $message =  str_replace("%name", $list['name'].' '.$this->follow_name.' ', $this->message);
                 }
                 if ($config['0098'] && $config['0098']['active']){
                     $endpoint = "http://www.0098sms.com/sendsmslink.aspx";
-                    Http::get($endpoint,[
-                        'FROM' => $config['0098']['sender'],
-                        'TO' => $list['mobile'],
-                        'TEXT' => trim($message),
-                        'USERNAME' => $config['0098']['user_name'],
-                        'PASSWORD' => $config['0098']['password'],
-                        'DOMAIN' => '0098',
-                    ]);
+//                    Http::get($endpoint,[
+//                        'FROM' => $config['0098']['sender'],
+//                        'TO' => $list['mobile'],
+//                        'TEXT' => trim($message),
+//                        'USERNAME' => $config['0098']['user_name'],
+//                        'PASSWORD' => $config['0098']['password'],
+//                        'DOMAIN' => '0098',
+//                    ]);
+                    $client = new \GuzzleHttp\Client();
+                    $response = $client->request('GET', $endpoint, [
+                        'query' => [
+                            'FROM' => $config['0098']['sender'],
+                            'TO' => $list['mobile'],
+                            'TEXT' => trim($message),
+                            'USERNAME' => $config['0098']['user_name'],
+                            'PASSWORD' => $config['0098']['password'],
+                            'DOMAIN' => '0098',
+                        ]]);
                     $this->type = '0098';
                     $this->store($list,$message);
                 }
@@ -71,11 +78,31 @@ class MessageInterface
 
         }
         return true;
-
-//        return response()->json($message);
     }
 
-    protected function store($list,$message){
+
+
+    /**
+     * send message with data
+     *
+     * @return bool
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function createMessage(){
+        $this->sendMesssage($this->list_send);
+        return true;
+    }
+
+
+
+    /**
+     * store data in database
+     *
+     * @param $list
+     * @param $message
+     * @return void
+     */
+    protected function store($list, $message){
         Message::create([
             'customer_id' => $list['customer_id'] ?? null,
             'name'        => $list['name'] ?? null,
@@ -89,9 +116,14 @@ class MessageInterface
 
 
 
-
-/////table
-    protected function addLimit($table,$list){
+    /**
+     * add page and limit for show data
+     *
+     * @param $table
+     * @param $list
+     * @return mixed
+     */
+    protected function addLimit($table, $list){
         if (!isset($list['page'])){
             $list['page'] = 0;
         }
@@ -110,8 +142,16 @@ class MessageInterface
         return $list;
     }
 
+
+
+    /**
+     * show all data with search and filter
+     *
+     * @param $data
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index($data){
-        $message = Message::query();
+        $message = Message::orderBy('id','desc');
         if (isset($data['filters'])){
             $this->filter($message,$data['filters']);
         }
@@ -122,7 +162,16 @@ class MessageInterface
 
     }
 
-    protected function filter(&$message,$filters){
+
+
+    /**
+     * filter data
+     *
+     * @param $message
+     * @param $filters
+     * @return void
+     */
+    protected function filter(&$message, $filters){
         foreach ($filters as $filter){
             if ($filter['field'] == 'name'){
                 $message->where('name','like', '%' . $filter['value'] . '%');
@@ -145,48 +194,35 @@ class MessageInterface
         }
     }
 
-    protected function search(&$message,$search){
+
+
+    /**
+     * search in data
+     *
+     * @param $message
+     * @param $search
+     * @return void
+     */
+    protected function search(&$message, $search){
         $message->where('name', 'like' , '%' . $search . '%')
             ->Orwhere('message' ,'like', '%' . $search . '%')
             ->Orwhere('mobile','like','%'.$search.'%');
     }
+
+
+
+    /**
+     * get excel file and send message and store data
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function insertExcel(Request $request){
         $array = Excel::toArray(new MessageImport(), $request->file('file')->store('temp'));
-        $this->createMessageForExcel($array);
+        $this->sendMesssage($array[0]);
         return response()->json(true);
     }
-    public function createMessageForExcel($array){
-        $config = Arr::get(config(),'message',false);
-        if ($config){
-            foreach ($array[0] as $list){
-                if (!isset($list['name'])){
-                    $message =  str_replace("%name", $this->swap_name.' '.$this->follow_name.' ', $this->message);
-                }else{
-                    $message =  str_replace("%name", $list['name'].' '.$this->follow_name.' ', $this->message);
-                }
-                if ($config['0098'] && $config['0098']['active']){
-                    $endpoint = "http://www.0098sms.com/sendsmslink.aspx";
-                    Http::get($endpoint,[
-                        'FROM' => $config['0098']['sender'],
-                        'TO' => $list['mobile'],
-                        'TEXT' => trim($message),
-                        'USERNAME' => $config['0098']['user_name'],
-                        'PASSWORD' => $config['0098']['password'],
-                        'DOMAIN' => '0098',
-                    ]);
-                    $this->type = '0098';
-                    $this->store($list,$message);
-                }
-                elseif ($config['kavenegar'] && $config['kavenegar']['active']){
-                    $api = new KavenegarApi($config['kavenegar']['api_key']);
-                    $results = $api->Send($config['kavenegar']['sender'],$list['mobile'], $message);
-                    $this->type = 'kavenegar';
-                    $this->store($list,$message);
-                }
-            }
 
-        }
-        return true;
-    }
+
 
 }
